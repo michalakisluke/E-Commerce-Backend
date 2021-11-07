3b98596314835f8109ddfda3f77b974a90dba544
Luke Michalakis <michalakisluke@gmail.com>
2021-11-06 20:04:33 -0400

    <Build product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,8 +5,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  console.log('===============');
+  Product.findAll({
+      attributes: [
+          'id', 
+          'product_name', 
+          'price', 
+          'stock',
+          'category_id',
+          [sequelize.literal(''), '']
+      ],
+      order: [],
+      include: []
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 // get one product
